d19c48674a5a0075e79f397a958fcdb3000048f8
threeaccents <hirethreeaccents@gmail.com>
2019-03-17 12:39:39 -0600

    initial create venue view

--- src/components/views/CreateVenueView/CreateVenueView.tsx
+++ src/components/views/CreateVenueView/CreateVenueView.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col, Steps, Divider } from 'antd';
 
 import CreateVenueInformationSection from './_sections/CreateVenueInformationSection'
+import { VenueInformationState } from './interface';
 
 const Step = Steps.Step;
 
@@ -10,19 +11,54 @@ const
     AMENITIES_STEP = 'AMENITIES_STEP'
 
 
+const initialInformationState = {
+    name: '',
+    listingName: '',
+    commission: 0,
+    roomName: '',
+    email: '',
+    phoneNumber: '',
+    phoneExt: '',
+    contactName: '',
+    description: '',
+    venueType: '',
+    eventType: '',
+    googleReviewRating: 0,
+    googleReviewQty: 0,
+    address: '',
+    city: '',
+    neighbourhood: '',
+    fullAddress: '',
+}
+
+
 interface State {
-    step: string
+    step: string,
+    informationState: VenueInformationState;
 }
 
 class CreateVenueView extends Component {
     state: State = {
-        step: INFO_STEP
+        step: INFO_STEP,
+        informationState: initialInformationState,
+    }
+
+    updateInfoState = (key: string, value: string | number) => {
+        const informationState = { ...this.state.informationState, [key]: value }
+        this.setState({
+            informationState
+        })
     }
 
     private displaySteps() {
         switch (this.state.step) {
             case INFO_STEP:
-                return  <CreateVenueInformationSection />
+                return (
+                    <CreateVenueInformationSection
+                        data={this.state.informationState}
+                        updateState={this.updateInfoState}
+                        next={() => this.setState({step: 2})} />
+                )
             case AMENITIES_STEP:
         }
     }
