960eb0c3927e09c68f8e07dbf266297406c60538
robbieve <robbieeve751994@gmail.com>
2019-03-18 06:58:23 -0600

    add skeleton for creating event gallery

---
+++ src/screens/CreateEventGallery/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class CreateEventGallery extends Component {
+    render() {
+        return (
+            <div><h1>Event Gallery!</h1></div>
+        )
+    }
+}
+
+export default CreateEventGallery;
